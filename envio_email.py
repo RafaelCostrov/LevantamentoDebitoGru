7063345fba80dd73a0c96901d2b7d76c783e3912
Rafael Costrov <inov2@controller-oraculus.com.br>
2025-04-17 16:57:01 -0300

    Ajustando para funcionar em qualquer pc

--- envio_email.py
+++ envio_email.py
@@ -8,8 +8,10 @@ from email.mime.multipart import MIMEMultipart
 
 
 load_dotenv()
-SERVICE_ACCOUNT_FILE = os.getenv('SERVICE_ACCOUNT_FILE')
-SCOPES = os.getenv('SCOPES_EMAIL').split(',')  
+base_dir = os.path.dirname(os.path.abspath(__file__))
+credentials_filename = os.getenv("SERVICE_ACCOUNT_FILE")
+SERVICE_ACCOUNT_FILE = os.path.join(base_dir, credentials_filename)
+SCOPES = os.getenv('SCOPES_EMAIL').split(',')
 EMAIL_USER = os.getenv('EMAIL_USER')
 TEMPLATE_PATH = os.getenv('TEMPLATE_PATH')
 
@@ -18,10 +20,12 @@ credentials = service_account.Credentials.from_service_account_file(
 ).with_subject(EMAIL_USER)
 service = build('gmail', 'v1', credentials=credentials)
 
+
 def carregar_template(nome, link):
     with open(TEMPLATE_PATH, 'r', encoding='utf-8') as file:
         template = file.read()
         return template.replace("{0}", nome).replace("{1}", link)
+
 
 def criar_email(destinatario, assunto, nome, link):
     email = MIMEMultipart()
@@ -29,14 +33,15 @@ def criar_email(destinatario, assunto, nome, link):
     email['from'] = EMAIL_USER
     email['subject'] = assunto
     email.attach(MIMEText(carregar_template(nome, link), 'html'))
-    
+
     raw_message = base64.urlsafe_b64encode(email.as_bytes()).decode('utf-8')
     return {'raw': raw_message}
+
 
 def enviar(destinatario, assunto, nome, link):
     email = criar_email(destinatario, assunto, nome, link)
     service.users().messages().send(
-        userId='me',  
+        userId='me',
         body=email
     ).execute()
     print(f"E-mail enviado com sucesso para {destinatario}!")
--- robo_gru.py
+++ robo_gru.py
@@ -2,7 +2,6 @@ import datetime
 import os
 import time
 import base64
-import undetected_chromedriver as uc
 from dotenv import load_dotenv
 from servico_google import acessando_sheets
 from envio_drive import salvar_drive
@@ -28,11 +27,13 @@ coluna_resp = guarulhos.col_values(3)[1:]
 coluna_cnpj = guarulhos.col_values(4)[1:]
 
 chrome_options = Options()
-chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
+chrome_options.add_argument(
+    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
 chrome_options.add_argument("--disable-blink-features=AutomationControlled")
 chrome_options.add_argument("--incognito")
 chrome_options.add_argument("--headless")
-chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_experimental_option(
+    "excludeSwitches", ["enable-automation"])
 SERVICE = os.getenv('SERVICE')
 servico = Service(SERVICE)
 navegador = webdriver.Chrome(service=servico, options=chrome_options)
@@ -46,50 +47,58 @@ responsaveis_com_pasta = set()
 for i, (cnpj, resp, nome) in enumerate(zip(coluna_cnpj, coluna_resp, coluna_nome), start=2):
     while True:
         try:
-            navegador.get("https://fazenda.guarulhos.sp.gov.br/ords/guarulho/f?p=628:9")
+            navegador.get(
+                "https://fazenda.guarulhos.sp.gov.br/ords/guarulho/f?p=628:9")
             navegador.maximize_window()
-            extrato_debito = navegador.find_element(By.XPATH, '//*[@id="P9_EXTRATO_DEBITO"]/div/div[2]/div/a')
+            extrato_debito = navegador.find_element(
+                By.XPATH, '//*[@id="P9_EXTRATO_DEBITO"]/div/div[2]/div/a')
             extrato_debito.click()
 
-            tipo_inscricao = navegador.find_element(By.XPATH, '//*[@id="P101_TIPO_INSCRICAO"]')
+            tipo_inscricao = navegador.find_element(
+                By.XPATH, '//*[@id="P101_TIPO_INSCRICAO"]')
             tipo_inscricao.click()
             tipo_inscricao.send_keys("m", Keys.ENTER)
 
-            campo_cnpj = navegador.find_element(By.XPATH, '//*[@id="P101_PESQUISA"]')
+            campo_cnpj = navegador.find_element(
+                By.XPATH, '//*[@id="P101_PESQUISA"]')
             campo_cnpj.clear()
             campo_cnpj.send_keys(cnpj)
 
-            botao_pesquisar = navegador.find_element(By.XPATH, '//*[@id="P101_BTN_PESQUISA"]')
+            botao_pesquisar = navegador.find_element(
+                By.XPATH, '//*[@id="P101_BTN_PESQUISA"]')
             botao_pesquisar.click()
             navegador.implicitly_wait(2)
 
             try:
-                numero_inscricao = navegador.find_element(By.XPATH, '//*[@id="report_P101_REL_PESQUISA"]/tbody/tr[2]/td/table/tbody/tr[2]/td[1]')
+                numero_inscricao = navegador.find_element(
+                    By.XPATH, '//*[@id="report_P101_REL_PESQUISA"]/tbody/tr[2]/td/table/tbody/tr[2]/td[1]')
                 numero_inscricao.click()
             except Exception as e:
-                botao_home = navegador.find_element(By.XPATH, '//*[@id="container"]/div[1]/div[1]/div[1]/a/div')
+                botao_home = navegador.find_element(
+                    By.XPATH, '//*[@id="container"]/div[1]/div[1]/div[1]/a/div')
                 botao_home.click()
                 print(f"{cnpj}: sem inscricao municipal")
                 break
-            
+
             WebDriverWait(navegador, 30).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="container"]/div[2]/table/tbody/tr/td[1]/div/div/table/tbody/tr[5]/td/table/tbody/tr[1]/td'))
+                EC.presence_of_element_located(
+                    (By.XPATH, '//*[@id="container"]/div[2]/table/tbody/tr/td[1]/div/div/table/tbody/tr[5]/td/table/tbody/tr[1]/td'))
             )
-                              
-                                         
-            totais_element = navegador.find_element(By.XPATH, "//td[contains(text(), 'Totais :')]//following-sibling::td")
+
+            totais_element = navegador.find_element(
+                By.XPATH, "//td[contains(text(), 'Totais :')]//following-sibling::td")
             valor_totais = totais_element.text
             print(valor_totais)
 
             if valor_totais != "":
                 try:
-                
+
                     dia = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
                     nome_arquivo = f"{nome} - {cnpj} - {dia}.pdf"
 
                     print_options = PrintOptions()
-                    print_options.shrink_to_fit = True  
-                    print_options.orientation = 'portrait'        
+                    print_options.shrink_to_fit = True
+                    print_options.orientation = 'portrait'
                     pdf_content = navegador.print_page(print_options)
 
                     try:
@@ -107,32 +116,35 @@ for i, (cnpj, resp, nome) in enumerate(zip(coluna_cnpj, coluna_resp, coluna_nome
                     print(f"Erro inesperado: {e}")
 
                 if os.path.exists(nome_arquivo):
-                    pasta_id = salvar_drive(nome_arquivo, resp)  
+                    pasta_id = salvar_drive(nome_arquivo, resp)
                     responsaveis_com_pasta.add((resp, pasta_id))
                     time.sleep(5)
-                    os.remove(nome_arquivo)  
+                    os.remove(nome_arquivo)
                 else:
                     print("Erro: O arquivo PDF não foi gerado corretamente.")
-                    
+
             else:
                 print("Empresa com valor de debito zerado: " + nome)
-                               
-            botao_home = navegador.find_element(By.XPATH, '//*[@id="container"]/div[1]/div[1]/div[1]/a/div')
+
+            botao_home = navegador.find_element(
+                By.XPATH, '//*[@id="container"]/div[1]/div[1]/div[1]/a/div')
             botao_home.click()
-            botao_sair = navegador.find_element(By.XPATH, '//*[@id="P9_DADOS"]/div/table/tbody/tr[1]/td[3]/a')
+            botao_sair = navegador.find_element(
+                By.XPATH, '//*[@id="P9_DADOS"]/div/table/tbody/tr[1]/td[3]/a')
             botao_sair.click()
             break
-        
+
         except Exception as e:
             print(f"Erro ao processar {cnpj}: {e}")
             navegador.close()
             time.sleep(1)
             navegador.quit()
             time.sleep(3)
-            navegador = webdriver.Chrome(service=servico, options=chrome_options)  
-            
-    print(responsaveis_com_pasta)    
-     
+            navegador = webdriver.Chrome(
+                service=servico, options=chrome_options)
+
+    print(responsaveis_com_pasta)
+
 AMANDA = os.getenv('AMANDA')
 AMANDA_O = os.getenv('AMANDA_O')
 DANIELA_VIVIANE = os.getenv('DANIELA_VIVIANE')
@@ -144,6 +156,7 @@ DEFAULT = os.getenv('DEFAULT')
 for responsavel, id in responsaveis_com_pasta:
     link_pasta = f'https://drive.google.com/drive/folders/{id}'
     mes_atual = datetime.datetime.now().strftime("%m/%Y")
+
     def obter_email(responsavel):
         match responsavel:
             case "AMANDA":
@@ -160,7 +173,8 @@ for responsavel, id in responsaveis_com_pasta:
                 return TATIANE
             case _:
                 return DEFAULT
-    enviar(obter_email(responsavel), f'Levantamento de Debito - {mes_atual}', responsavel.title(), link_pasta)
+    enviar(obter_email(responsavel),
+           f'Levantamento de Debito - {mes_atual}', responsavel.title(), link_pasta)
 
 navegador.close()
 time.sleep(1)
